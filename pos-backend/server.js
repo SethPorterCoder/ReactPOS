84242e166f0633ce461a41b2384beba30865cdf4
SethPorterCoder <SethPorterWork@gmail.com>
2024-08-06 21:40:45 -0500

    Initial commit

---
+++ pos-backend/server.js
@@ -0,0 +1,81 @@
+//Start this server by running 'node server.js' in the directory of this file.
+
+// server.js 
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+//Connect to the server by using localhost with 3001 as the port.
+
+app.use(cors());
+app.use(express.json()); // To parse JSON bodies
+
+// Create a connection to the database
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'admin',
+  password: 'admin',
+  database: 'reactPOS'
+});
+
+// Connect to the database
+//db.connect will try to connect to the DB sever, if there is an error, it will return
+// an error to the err variable through a lambda function.  If no error is found
+// and the connection is established, then it will return a null.
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
+
+
+
+//DB query test
+db.query('select * from users', (error, results) => {
+  console.log(results);
+  
+  for(var i = 0; i < results.length; i++) {
+    console.log(results[0].fName);
+    console.log(results[0].lName);
+  }
+
+
+});
+
+
+
+
+
+
+//Arrow or Lambda functions
+// Parameter => statements
+// Parameters we pass through => The statements we run that have access to those paramters
+
+
+app.post('/api/login', (req, res) => {
+  console.log(req.body);
+  db.execute("select * from users where userID = ? and userPassword = ?", [req.body["ID"], req.body["PIN"]], (err, results) => {
+    if(err) {
+      throw err;
+    } else {
+      console.log("Results" + results);
+      res.send(results);
+    }
+  });
+
+});
+
+app.post('/api/message', (req, res) => {
+  const message = req.body.message;
+  console.log('Received message:', message);
+  res.send(message);
+});
+
+// Start the server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ pos-frontend/src/App.tsx
@@ -0,0 +1,180 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React, { useState, useEffect, useRef, ObjectHTMLAttributes, KeyboardEventHandler } from 'react';
+import axios from 'axios';
+//You need axios to make HTTP requests!
+
+function App() {
+  return (
+    <div className="App">
+      <LogIn></LogIn> {/* This is how you call components!*/}
+    </div>
+  );
+}
+
+
+
+function LogIn() {
+  //This is how to make reactive button with states
+  //States allow for the visible components on the website to change.
+  //There are two states.  One that user sees called is the actual DOM.
+  //The other is called the virtual DOM.  Changes made to the virtual DOM update the actual DOM.
+  //This is super fast compared to editing the acutal DOM.  ReactJS is all about changing the virutal DOM over the actual DOM.
+  const [test, changeLabel] = useState("Nothing has been pressed yet");
+  const [textData, setTextData] = useState("No request has been made");
+
+  const [userIDValue, setUserIDValue] = useState("");
+  const [pinValue, setPinValue] = useState("");
+
+  //Hooks allow you to create refences to tags.  In this case, it's for input tag to change focus
+  const pinRef = useRef<HTMLInputElement>(null);
+
+
+  
+  //The axios.get code attemps a connection with the https server.  
+  //If a successful request connection is formed, the .then code is ran and the data will be returned to the response variable.
+  //the .data is the variable that is stored in the response.
+  //For post methods, aysnc must be declared as you need to wait for the axios post promise to be fulifulled.
+  const sendAPI = async () => {
+    try {
+      //With post, you need to await the completion of the promise as Javascript code runs all at once!
+      //Meaning you need to declare when to wait when this promise is completed or it not work correctly.
+      const response = await axios.post('http://localhost:3001/api/message', "Hello world");
+      console.log('Server response:', response.data);
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  }
+
+  const submitLogIn = async (ID: string, PIN: string) => {
+    try {
+      let userCred = {"ID":parseInt(ID), "PIN":parseInt(PIN)}
+        
+  
+      const response = await axios.post("http://localhost:3001/api/login", userCred);
+      console.log(response.data);
+    } catch(error){
+      console.error("Error Message:" + error);
+    }
+  }
+
+
+
+
+
+
+  //When the user ID input changes in anyway at all, it calls this function via the onChange tag in the input form
+  const userIDValueEvent = (event: {target: {value: string}}) => {
+    setUserIDValue(event.target.value);
+  } 
+
+  const pinValueEvent = (event: {target: {value: string}}) => {
+    setPinValue(event.target.value);
+  }
+
+
+  //This is called when the user presses down on any key via the keyDown tag.
+  //Since the method activates with a keyboard event, we have to cast event to it with the HTMLInputElement Generic
+  const handleKeyDownForm = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+
+    //Cast the event as an HTMLInput Element
+    //This is because it allows the specific Input attribute to be accessed
+    const inputElement = event.target as HTMLInputElement;
+
+    switch(inputElement.placeholder) {
+      case("ID"): 
+        switch(event.key) {
+          case("Enter"):
+            event.preventDefault(); //This prevents the webpage from doing it's default action.  IN this case, it's refreshing the page.
+            pinRef.current?.focus(); //This changes the active form to the pin code form.
+            //The ? in TypeScript allows for the compiler to ONLY run this code if it is not null.
+            return;
+        }
+
+      case("PIN"):
+        switch(event.key) {
+          case("Enter"):
+          event.preventDefault(); //This prevents the webpage from doing it's default action.  IN this case, it's refreshing the page.
+          buttonPressed();
+          return;
+        }
+    }
+  }
+
+  const buttonPressed = async () => {
+    changeLabel("Button was pressed")
+    //sendAPI();
+    submitLogIn(userIDValue, pinValue);
+
+  }
+
+  //Hash method for password to sende to the backend.  It uses SHA-256 encoding
+  const hashString = async () => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode("password");
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    
+    //console.log(hashHex);
+
+  }
+  hashString();
+
+
+
+
+
+
+
+  return (
+    <div className="log">
+      <h1>Sign in</h1>
+
+      <form>
+      {/* The value tag is how you set text in code. */}
+        <input 
+          type="text" 
+          placeholder="ID" 
+          value={userIDValue} 
+          onChange={userIDValueEvent}
+          onKeyDown={handleKeyDownForm}>
+        </input> 
+      </form>
+
+      <form>
+        {/* The value tag is how you set text in code. */}
+        <input 
+          type="text" 
+          placeholder="PIN" 
+          value={pinValue} 
+          onChange={pinValueEvent}
+          onKeyDown={handleKeyDownForm}
+          ref={pinRef}>
+        </input>
+      </form>
+
+      <form>
+        <button type="button" onClick={buttonPressed}>Enter</button>
+      </form>
+
+      {/* Line break */}
+      <br>
+      </br>
+
+      <label>{test}</label>
+
+      {/* Line break */}
+      <br> 
+      </br>
+
+      <label>{textData}</label>
+
+    </div>
+  )
+
+
+}
+
+export default App
